a22c2bee3d7351d46e40aa4531430311b1750871
Christof Weigandt <christof.weigandt@gmail.com>
2018-04-25 21:50:57 +0200

    two days work

--- app/src/main/java/info/weigandt/goalacademy/MainActivity.java
+++ app/src/main/java/info/weigandt/goalacademy/MainActivity.java
@@ -1,13 +1,52 @@
 package info.weigandt.goalacademy;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import info.weigandt.goalacademy.adapters.FixedTabsFragmentPagerAdapter;
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity {
+    FixedTabsFragmentPagerAdapter mFixedTabsFragmentPagerAdapter;
+    @BindView(R.id.viewpager) ViewPager mViewPager;
+    @BindView(R.id.tablayout) TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Butterknife
+        ButterKnife.bind(this);
+
+        // Initialize Timber
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            // Online-crash-reporting may be added here later on for release versions
+        }
+        // Create an adapter that knows which fragment should be shown on each page
+        mFixedTabsFragmentPagerAdapter = new FixedTabsFragmentPagerAdapter(getSupportFragmentManager(), this);
+        // Set the adapter onto the view pager
+        mViewPager.setAdapter(mFixedTabsFragmentPagerAdapter);
+
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        // TODO maybe needed at some point?
+        //adapter.startUpdate(viewPager);
+        //tab0 = (Fragment0) adapter.instantiateItem(viewPager, 0);
+        //tab1 = (Fragment1) adapter.instantiateItem(viewPager, 1);
+        //adapter.finishUpdate(viewPager);
+
     }
 }
---
+++ app/src/main/java/info/weigandt/goalacademy/adapters/FixedTabsFragmentPagerAdapter.java
@@ -0,0 +1,51 @@
+package info.weigandt.goalacademy.adapters;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import info.weigandt.goalacademy.R; // TODO try to remove this later on
+import info.weigandt.goalacademy.fragments.TrackFragment;
+
+public class FixedTabsFragmentPagerAdapter extends FragmentPagerAdapter {
+    private final Context mContext;
+
+    public FixedTabsFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;   // TODO use value from config or so?
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // Do NOT try to save references to the Fragments in getItem(),
+        // because getItem() is not always called. If the Fragment
+        // was already created, then it will be retrieved from the FragmentManger
+        // and not here (i.e. getItem() won't be called again).
+        switch (position) {
+            case 0:
+                return TrackFragment.newInstance(null, null);  // TODO enter correct fragment
+            case 1:
+                return new Fragment();// TODO enter correct fragment
+            case 2:
+                return new Fragment();// TODO enter correct fragment
+            default:
+                return null;
+        }
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getResources().getString(R.string.category_track);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_track); // TODO enter tab sections here
+        } else {
+            return mContext.getString(R.string.category_track); // TODO enter tab sections here
+        }
+    }
+}
